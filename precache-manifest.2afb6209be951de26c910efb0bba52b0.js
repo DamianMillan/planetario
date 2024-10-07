bcd47fb1a1cec64c1396c3c43f24551becf3a0a1
DamianMillan <128572600+DamianMillan@users.noreply.github.com>
2024-10-07 00:47:47 -0700

    Add files via upload

---
+++ precache-manifest.2afb6209be951de26c910efb0bba52b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb21dd6a3ea92fe12421cd0ec24f4135",
+    "url": "/Planetarium-3Dweb/index.html"
+  },
+  {
+    "revision": "23a2046782923d56c164",
+    "url": "/Planetarium-3Dweb/static/css/main.c66fc559.chunk.css"
+  },
+  {
+    "revision": "00fd579e3670e0bf8d3f",
+    "url": "/Planetarium-3Dweb/static/js/2.1934cc0b.chunk.js"
+  },
+  {
+    "revision": "50944ac833af085f88ddda39b257ffd3",
+    "url": "/Planetarium-3Dweb/static/js/2.1934cc0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a85546a72b3551bc2dfd",
+    "url": "/Planetarium-3Dweb/static/js/3.f893e0ed.chunk.js"
+  },
+  {
+    "revision": "23a2046782923d56c164",
+    "url": "/Planetarium-3Dweb/static/js/main.f73e8f83.chunk.js"
+  },
+  {
+    "revision": "918f21558f06ac854dfa",
+    "url": "/Planetarium-3Dweb/static/js/runtime-main.b62780b8.js"
+  }
+]);
